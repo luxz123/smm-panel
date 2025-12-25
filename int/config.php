06f14b4d041f618c79a5442462279a67e7fdbd88
luxz123 <luxzzhicihezhi@gmail.com>
2025-12-25 11:14:14 +0700

    initial clean upload

---
+++ int/config.php
@@ -0,0 +1,23 @@
+<?php
+
+
+
+    define('PATH', realpath('.'));
+    define('SUBFOLDER', false);
+    define('URL', 'https://smmsaom.xyz');
+       define('ADMIN_URL', 'admin');//dont change if dont know about that
+
+   define('SCRIPTAPI', '');//dont change if dont know about that
+    ini_set('display_errors', 0);
+    date_default_timezone_set('Asia/Kolkata');
+    
+    return [
+      'db' => [
+        'name'    =>  'smmsaom1_smm',
+        'host'    =>  'localhost',
+        'user'    =>  'smmsaom1_smm',
+        'pass'    =>  'FQz[c_aRl)N-',
+        'charset' =>  'utf8mb4' 
+      ]
+    ];
+    
